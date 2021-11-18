fcea690e458f7f4c4041f6b8c10a9e2728aaba76
Kevin Lane <59802468+Kevinclane@users.noreply.github.com>
2021-11-18 14:14:45 -0700

    deploy

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+# abort on errors
+set -e
+# build
+cd clientsrc
+npm run build
+cd ..
+# navigate into the build output directory
+cd client
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+# git init
+git add -A
+git commit -m 'deploy'
+git push -f https://github.com/Kevinclane/landingpagevue master:gh-pages
+cd -
